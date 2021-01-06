c817d224bb171bae6e3a6f5c55e2862788259c08
stilus-solutions <68062493+stilus-solutions@users.noreply.github.com>
2021-01-06 21:56:56 +1100

    Added video
    Spent a bit more time on the website adding content and styling.

--- js/scripts.js
+++ js/scripts.js
@@ -17,4 +17,26 @@
   		hidePreloader();
   	});
 
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+  	$(function() {
+  		$(document).on('click', 'a.page-scroll', function(event) {
+  			var $anchor = $(this);
+  			$('html, body').stop().animate({
+  				scrollTop: $($anchor.attr('href')).offset().top
+  			}, 600, 'easeInOutExpo');
+  			event.preventDefault();
+  		});
+  	});
+
+    $(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 50) {
+            $(".ss_nav").addClass("navbar-custom-active");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+           $(".ss_nav").removeClass("navbar-custom-active");
+        }
+    });
+});
+
 })(jQuery);
